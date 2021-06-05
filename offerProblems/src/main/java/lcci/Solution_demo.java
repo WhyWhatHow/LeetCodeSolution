e64dbf2b66b96b98d8f985d760a6f86d00442e3a
whywhathow <anothernash@126.com>
2021-06-05 22:44:57 +0800

    lru算法: 程序员面试金典 : 数组,链表字符串,

---
+++ offerProblems/src/main/java/lcci/Solution_demo.java
@@ -0,0 +1,18 @@
+package lcci;
+
+/**
+ * @program: LeetCodeSolution
+ * @description:
+ * @author: WhyWhatHow
+ * @create: 2020-05-07 12:09
+ **/
+
+public class Solution_demo {
+
+    public static void main(String[] args) {
+        Solution_demo sol = new Solution_demo();
+        System.out.println("==================");
+    }
+}
+
+
---
+++ offerProblems/src/main/java/leetcode/algorithm/demo/Solution_213.java
@@ -0,0 +1,23 @@
+package leetcode.algorithm.demo;
+
+/**
+ * @program: LeetCodeSolution
+ * @description:
+ * @author: WhyWhatHow
+ * @create: 2020-05-07 12:09
+ **/
+
+public class Solution_213 {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Solution_213 sol = new Solution_213();
+        System.out.println("==================");
+    }
+}
+
+
