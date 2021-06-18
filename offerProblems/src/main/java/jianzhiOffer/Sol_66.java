b9dd02bd61f2cd0d6b5557d696a6c9db407098a6
whywhathow <anothernash@126.com>
2021-06-18 22:21:55 +0800

    剑指offer: 位运算,归并排序求数组逆序对,+数学整数拆分

---
+++ offerProblems/src/main/java/jianzhiOffer/Sol_66.java
@@ -0,0 +1,20 @@
+package jianzhiOffer;
+
+/**
+ * @program: LeetCodeSolution
+ * @description:
+ * @author: WhyWhatHow
+ * @create: 2020-05-22 08:10
+ **/
+public class Sol_66 {
+    public int[] constructArr(int[] a) {
+        int[] ans = new int[a.length];
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Sol_66 sol = new Sol_66();
+        System.out.println("=======");
+    }
+}
