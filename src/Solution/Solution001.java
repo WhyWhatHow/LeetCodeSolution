4c00a4476f5cbc4477db2e4b9edc39450e28e70e
whywhathow <whywhathow.fun@gmail.com>
2022-07-26 16:17:59 +0800

    test git push

--- src/Solution/Solution001.java
+++ src/Solution/Solution001.java
@@ -4,7 +4,33 @@ import java.util.HashMap;
 
 public class Solution001 {
     public static void main(String[] args) {
+        System.out.println(4<<1);
+        System.out.println(4>>1);
+        System.out.println(4>>>1);
+        HashMap<String,Object> map =new HashMap<>();
+        map.put("abc",1);
+        map.put("abc1",2);
+        map.put("abc2",3);
+        map.put("abc3",4);
+        map.put("abc4",5);
+        map.put("abc5",7);
+        map.put("abc6",8);
+        map.put("abc7",9);
+        map.put("abc8",10);
+        map.put("abc9",11);
+        map.put("abc0",12);
+        map.put("abc77",13);
+        map.put("abc55",14);
+        map.put("abc66",16);
+
+
+
+
+
+
+
         System.out.println("你好,世界");
     }
+
 
 }
