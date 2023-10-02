489d03b57010356731e52e6039d907a866d6fa8e
whywhathow <whywhathow.fun@gmail.com>
2023-10-02 10:05:19 +0800

    #pq

---
+++ offerProblems/src/main/java/leetcode/algorithm/demo/Solution_215.java
@@ -0,0 +1,41 @@
+package leetcode.algorithm.demo;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @program: LeetCodeSolution
+ * @description:
+ * @author: WhyWhatHow
+ **/
+
+public class Solution_215 {
+
+    public static void main(String[] args) {
+        Solution_215 sol = new Solution_215();
+        System.out.println("==================");
+        System.out.println(sol.findKthLargest(new int[]{
+                        3,2,1,5,6,4
+                },
+                2));
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int res = 0;
+        for (int num : nums) {
+            pq.add(num);
+        }
+        while (!pq.isEmpty()) {
+            Integer poll = pq.poll();
+            k--;
+            if (k == 0) {
+                res = poll;
+                break;
+            }
+        }
+        return res;
+    }
+}
+
+
