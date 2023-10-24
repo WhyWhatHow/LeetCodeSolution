cb4ee50aa68ab2b15425aec9b46067c6870df70c
whywhathow <whywhathow.fun@gmail.com>
2023-10-24 22:59:27 +0800

    #pq #slide windows #hard

--- offerProblems/src/main/java/leetcode/algorithm/Solution_239.java
+++ offerProblems/src/main/java/leetcode/algorithm/Solution_239.java
@@ -1,8 +1,8 @@
 package leetcode.algorithm;
 
-import com.sun.jmx.remote.internal.ArrayQueue;
-
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
 
 /**
  * @program: LeetCodeSolution
@@ -11,16 +11,37 @@ import java.util.*;
  * @create: 2020-05-07 12:09
  **/
 public class Solution_239 {
+    // #ac
+    // pq 降序堆| 大顶堆 {idx, nums[idx]} 入队
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // init
+        int cnt = 0;
+        int[] res = new int[nums.length - k + 1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        //init pq , left = i-k+1 , right = i
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(new int[]{i, nums[i]});
+
+            if (i >= k - 1) {
+                // remove idx < left elements (left  = i - k + 1)
+                while (!pq.isEmpty() && pq.peek()[0] < (i - k + 1)) pq.poll();
+                res[cnt++] = pq.peek()[1];
+
+            }
+        }
+
+        return res;
+    }
+
     /**
      * 求滑动窗口的最大值,线性时间内
      * 思路: 双端队列，保证队首是最大元素,
-     *
-     *
-     *
+     * #wa
      * @param nums
      * @return
      */
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow2(int[] nums, int k) {
         Deque<Integer> queue = new ArrayDeque(k);
         int[] ans = new int[nums.length - k + 1];
         int cnt = 0;
@@ -67,9 +88,9 @@ public class Solution_239 {
 
     public static void main(String[] args) {
         Solution_239 sol = new Solution_239();
-//        int[] temp = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        int[] temp = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
 //        int[] temp = new int[]{1,-1};
-        int[] temp = new int[]{1,3,1,2,0,5};
+//        int[] temp = new int[]{1, 3, 1, 2, 0, 5};
         int[] ints = sol.maxSlidingWindow(temp, 3);
 
         for (int anInt : ints) {
