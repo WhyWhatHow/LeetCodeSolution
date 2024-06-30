564822d9538e474b18cc16832185ed757a82a091
whywhathow <whywhathow.fun@gmail.com>
2024-06-30 22:32:56 +0800

    #dp #01

--- offer-algorithms/src/main/java/leetcode/algorithm/dp/Solution_494.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/dp/Solution_494.java
@@ -20,7 +20,45 @@ public class Solution_494 {
     }
 
     /**
-     * 0-1背包, dp
+     * 0-1背包
+     * p : all set of positive nums sum.
+     * q : 负数之和 的abs
+     * 则有:
+     * p+q = sum
+     * p-q = target
+     * p = (sum+target)/2  //  p>0 && p is even num.
+     * 设 f[i] 表示 正数子集和为i 的方案数.
+     * 则有 f[0] =1 ,
+     * 对于每一个num ,
+     * 选择num, f[i] += f[i-num]
+     * 不选择 num: 不变
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        // sum+target 为奇数, || sum< |target|
+        if (sum < Math.abs(target) || ((sum + target) & 1) != 0) return 0;
+
+        int p = (sum + target) / 2;
+        int[] f = new int[p + 1];
+        f[0] = 1; //
+
+        for (int num : nums) {
+            for (int i = f.length - 1; i >= num; i--) {
+                f[i] += f[i - num];
+            }
+        }
+
+        return f[p];
+    }
+
+    /**
+     * dp
      * if f[i][j] means : 前i个元素, sum == j 's num.
      * f[i][j] = max( f[i-1][j+nums[i]]+1, f[i-1][j-nums[i]] +1 ) // wa
      * f[i][j] = f[i-1][j+nums[i-1]]  + f[i-1][j-nums[i-1]]   // plus , minis
@@ -32,7 +70,7 @@ public class Solution_494 {
      * @param target
      * @return
      */
-    public int findTargetSumWays(int[] nums, int target) {
+    public int findTargetSumWaysDP(int[] nums, int target) {
         int sum = 0;
         for (int num : nums) {
             sum += num;
