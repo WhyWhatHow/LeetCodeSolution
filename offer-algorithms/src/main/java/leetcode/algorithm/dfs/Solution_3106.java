752af700b4414f8769476048a217ec7b0b6395b4
whywhathow <whywhathow.fun@gmail.com>
2024-07-27 09:53:47 +0800

    #dfs #medium

---
+++ offer-algorithms/src/main/java/leetcode/algorithm/dfs/Solution_3106.java
@@ -0,0 +1,58 @@
+package leetcode.algorithm.dfs;
+
+/**
+ * @program: LeetCodeSolution
+ * @description: #
+ * @author: WhyWhatHow
+ **/
+
+public class Solution_3106 {
+
+    public static void main(String[] args) {
+        Solution_3106 sol = new Solution_3106();
+        System.out.println(sol.distance('b', 'z'));
+        System.out.println(sol.getSmallestString(
+                "zbbz", 3
+        ));
+        System.out.println("==================");
+    }
+
+    public String getSmallestString(String s, int k) {
+        char[] chars = s.toCharArray();
+        char[] cs = new char[chars.length];
+        dfs(0, cs, chars, k);
+        return String.valueOf(cs);
+    }
+
+    char c = 'a';
+
+    boolean yes = false;
+
+    private void dfs(int cur, char[] cs, char[] chars, int k) {
+        if (k < 0) return;
+        if (cur == cs.length) {
+            yes = true;
+            return;
+        }
+        if (yes) return;
+        for (int i = 0; i < 26; i++) {
+            char tmp = (char) (c + i);
+            cs[cur] = tmp;
+            int val = distance(cs[cur], chars[cur]);
+            dfs(cur + 1, cs, chars, k - val);
+            if (yes) return;
+            cs[cur] = chars[cur];
+        }
+
+    }
+
+    int distance(char a, char b) {
+        int res = Math.abs(b - a);
+        if (res > 13) {
+            res = 26 - res;
+        }
+        return res;
+    }
+}
+
+
