5f5b7a07c89818c71927d72b56d66c1cae254ad3
whywhathow <whywhathow.fun@gmail.com>
2024-08-13 09:40:54 +0800

    feat: update binarysearch , support ceilSearch & floorSearch

--- offer-algorithms/src/main/java/dsa/MySearch.java
+++ offer-algorithms/src/main/java/dsa/MySearch.java
@@ -22,4 +22,40 @@ public class MySearch {
         return ans;
     }
 
+    /**
+     * [)  开区间写法
+     */
+    int search(int[] a, int target) {
+        int l = 0, r = a.length;
+        int mid = r / 2;
+        int res = -1;
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            if (a[mid] <= target) {
+                res = mid;
+                l = mid + 1;// 最大值 <=target
+            } else {
+                r = mid;
+//                res = mid;  // 最小值 >=target
+            }
+        }
+        return res;
+    }
+
+    //[] 闭区间, 二分查找
+    int search(int[] a, int target, int l, int r) {
+        int res = -1;
+        int mid = l + (r - l) / 2;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (a[mid] >= target) {
+                l = mid + 1;
+                res = mid; // min>=target
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+
 }
