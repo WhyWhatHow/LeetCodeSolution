57cf8f717ba8aff1b6ceda2ab5682d61ddcf41f3
whywhathow <whywhathow.fun@gmail.com>
2024-10-01 21:52:36 +0800

    feat: add #dfs with #memory_search solution for leetcode 983.

--- offer-algorithms/src/main/java/leetcode/algorithm/dp/Solution_983.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/dp/Solution_983.java
@@ -1,5 +1,7 @@
 package leetcode.algorithm.dp;
 
+import java.util.HashMap;
+
 /**
  * @program: LeetCodeSolution
  * @description:
@@ -10,13 +12,14 @@ public class Solution_983 {
 
     public static void main(String[] args) {
         Solution_983 sol = new Solution_983();
-        int i = sol.mincostTickets(new int[]{
+        int i = sol.mincostTicketsByDfs(new int[]{
 //                1, 4, 6, 7, 8, 20
-                6,8,9,18,20,21,23,25
+                6, 8, 9, 18, 20, 21, 23, 25
         }, new int[]{
 //                2, 7, 15
-                2,10,41
+                2, 10, 41
         });
+
         System.out.println(i);
         System.out.println("==================");
     }
@@ -37,11 +40,6 @@ public class Solution_983 {
         int idx = 0;
         for (int i = minDay; i <= maxDay; i++) {
             if (i == days[idx]) { // travel day
-//                getIDayMinCosts(costs, i, dp);
-//                dp[i] = Math.min(
-//                        Math.min(dp[i - 1] + costs[0], dp[Math.max(0, i - 7)] + costs[1]),
-//                        dp[Math.max(0, i - 30)] + costs[2]
-//                );
                 int x = dp[i - 1] + costs[0];
                 int y = dp[i < 7 ? 0 : i - 7] + costs[1];
                 int z = dp[i < 30 ? 0 : i - 30] + costs[2];
@@ -54,18 +52,39 @@ public class Solution_983 {
         return dp[maxDay];
     }
 
-    private void getIDayMinCosts(int[] costs, int curDay, int[] dp) {
-        if (1 <= curDay) {
-            dp[curDay] = Math.min(dp[curDay - 1] + costs[0], dp[curDay]);
+    public int mincostTicketsByDfs(int[] days, int[] costs) {
+        boolean[] vis = new boolean[days[days.length - 1]+1];
+        for (int day : days) {
+            vis[day] = true ;
         }
-        if (7 <= curDay) {
-            dp[curDay] = Math.min(dp[curDay - 7] + costs[1], dp[curDay]);
-        }
-        if (30 <= curDay) {
-            dp[curDay] = Math.min(dp[curDay - 30] + costs[2], dp[curDay]);
-        }
+        return dfs(days[days.length - 1], costs,vis);
+    }
 
+    HashMap<Integer,Integer> map =new HashMap<>();
+    /**
+     * dfs(i) , 表示第i 天的最低花费
+     * f[i] = min {
+     * f[i-1]+ costs[0],
+     * f[i-7] +costs[1],
+     * f[i-30] +costs[30]
+     * }
+     *
+     * @return
+     */
+    int dfs(int i, int[] costs, boolean[] vis) {
+        if (i <= 0) return 0;
+        // i  not in days
+        if(!vis[i]){
+//            vis[i] = true ;
+            return dfs(i-1, costs,vis);
+        }
+        if(map.containsKey(i))return map.get(i);
+        //  i in days
+        int res = dfs(i - 1, costs,vis) + costs[0];
+        res = Math.min(res, dfs(i - 7, costs,vis) + costs[1]);
+        res = Math.min(res, dfs(i - 30, costs,vis) + costs[2]);
+        map.put(i,res);
+        return res;
     }
 }
-
 
