28712ca8bb76685b2d838dd0e960b85c2522a4bd
whywhathow <whywhathow.fun@gmail.com>
2024-12-30 09:15:45 +0800

    refactor(dsa): opt buildTree() method , dsa.tree.TreeNode，-> TreeNode

--- offer-algorithms/src/main/java/leetcode/algorithm/dsa/TreeUtils.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/dsa/TreeUtils.java
@@ -13,21 +13,21 @@ import java.util.Queue;
  **/
 public class TreeUtils {
 
-    public static dsa.tree.TreeNode buildTree(Integer[] arr) {
+    public static TreeNode buildTree(Integer[] arr) {
         if (arr == null || arr.length == 0) {
             return null;
         }
 
-        dsa.tree.TreeNode root = new dsa.tree.TreeNode(arr[0]);
-        Queue<dsa.tree.TreeNode> queue = new LinkedList<>();
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
 
         int i = 1;
         while (!queue.isEmpty() && i < arr.length) {
-            dsa.tree.TreeNode current = queue.poll();
+            TreeNode current = queue.poll();
 
             if (i < arr.length && arr[i] != null) {
-                current.left = new dsa.tree.TreeNode(arr[i]);
+                current.left = new TreeNode(arr[i]);
                 queue.add(current.left);
             }
             i++;
