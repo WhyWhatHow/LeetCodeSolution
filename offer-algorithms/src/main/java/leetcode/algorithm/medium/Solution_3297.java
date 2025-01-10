573f14e3e4284d76b1a22f57729efee19d6cc6ea
whywhathow <whywhathow.fun@gmail.com>
2025-01-10 22:19:23 +0800

    #slide_window #medium

---
+++ offer-algorithms/src/main/java/leetcode/algorithm/medium/Solution_3297.java
@@ -0,0 +1,55 @@
+package leetcode.algorithm.medium;
+
+/**
+ * @program: LeetCodeSolution
+ * @description: #
+ * @author: WhyWhatHow
+ **/
+
+public class Solution_3297 {
+
+    public static void main(String[] args) {
+        Solution_3297 sol = new Solution_3297();
+//        System.out.println(sol.validSubstringCount("bcca", "abc"));
+//        System.out.println(sol.validSubstringCount("abcabc", "abc"));
+        System.out.println(sol.validSubstringCount("dcbdcdccb", "cdd"));
+        System.out.println("==================");
+    }
+
+    public long validSubstringCount(String word1, String word2) {
+        char[] cs = word1.toCharArray();
+        char[] css = word2.toCharArray();
+
+        int[] counts = new int[26]; // get word2 every char's count.
+        for (char c : css) {
+            counts[c - 'a']++;
+        }
+
+        int all = 0;
+        for (int count : counts) {
+            if (count > 0) all++;
+        }
+
+        int l = 0;
+        long res = 0;
+        for (int r = 0; r < cs.length; r++) {
+            char c = cs[r];
+            counts[c - 'a']--;
+            if (counts[c - 'a'] == 0) { // if char c not exists in word2 , counts[c] should less than 0
+                all--;
+            }
+
+            while (all == 0) {
+                char cc = cs[l++];
+                counts[cc - 'a']++;
+                if (counts[cc - 'a'] == 0) {
+                    all++;
+                }
+            }
+            res += l;
+        }
+
+        return res;
+
+    }
+}
