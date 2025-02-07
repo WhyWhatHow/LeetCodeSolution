6121f758d1d0be49b39ca39315cb1b07210d4233
whywhathow <whywhathow.fun@gmail.com>
2025-02-07 18:09:31 +0800

    feat(algorithms): 重构生成螺旋矩阵的算法
    - 新增了一种更简洁、高效的生成螺旋矩阵的方法
    - 使用方向数组简化了矩阵遍历逻辑
    - 优化了代码结构，提高了可读性和可维护性
    - 保留了旧的实现方法供参考

--- offer-algorithms/src/main/java/leetcode/algorithm/Solution_59.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/Solution_59.java
@@ -8,17 +8,42 @@ package leetcode.algorithm;
  **/
 
 public class Solution_59 {
+    int[] dir = new int[]{0, 1, 0, -1, 0};
+
     public int[][] generateMatrix(int n) {
+        int[][] as = new int[n][n];
+        int all = n * n;
+        int x = 0, y = 0;
+        int cnt = 1;
+        int d = 0; // cur direction
+        while (all >= cnt) {
+            as[x][y] = cnt++;
+            int dx = dir[d] + x;
+            int dy = dir[d + 1] + y;
+            if (dx < 0 || dx == n || dy < 0 || dy == n || as[dx][dy] != 0) { // change direction
+                d = (d + 1) % 4;
+                x = dir[d] + x;
+                y = dir[d + 1] + y;
+            } else {
+                x = dx;
+                y = dy;
+            }
+        }
+        return as;
+    }
+
+
+    public int[][] generateMatrixOld(int n) {
         int[][] ans = new int[n][n];
         int up = 0, down = n - 1, left = 0, right = n - 1;
         int all = n * n;
         int num = 1;
         while (num <= all) {
-            for (int i = left; i <=right; i++) {
+            for (int i = left; i <= right; i++) {
                 ans[up][i] = num++;
             }
             up++;
-            for (int i = up; i <=down; i++) {
+            for (int i = up; i <= down; i++) {
                 ans[i][right] = num++;
             }
             right--;
@@ -36,8 +61,8 @@ public class Solution_59 {
 
     public static void main(String[] args) {
         Solution_59 sol = new Solution_59();
-        int[][] ints = sol.generateMatrix(3);
-
+        int[][] ints = sol.generateMatrixOld(3);
+        System.out.println(sol.generateMatrix(9));
         System.out.println("==================");
     }
 }
