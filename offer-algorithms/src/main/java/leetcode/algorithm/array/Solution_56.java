4c87c70c46b7e69da4af112a80342f1a260a57be
whywhathow <whywhathow.fun@gmail.com>
2025-03-03 16:20:39 +0800

    feat(algorithm): 优化合并区间算法

--- offer-algorithms/src/main/java/leetcode/algorithm/array/Solution_56.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/array/Solution_56.java
@@ -1,5 +1,6 @@
 package leetcode.algorithm.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -17,9 +18,26 @@ public class Solution_56 {
         System.out.println("==================");
     }
 
-
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        ArrayList<int[]> s = new ArrayList<>();
+        s.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            // need merge [a,b] ,[c,d] ==> c>=a && c<b
+            if (s.getLast()[0] <= intervals[i][0] && intervals[i][0] <=s.getLast()[1]) {
+                int[] ints = s.removeLast();
+                s.addLast(new int[]{ints[0], Math.max(ints[1], intervals[i][1])});
+            } else {
+                s.addLast(intervals[i]);
+            }
+        }
+        int[][] ans = new int[s.size()][];
+        return s.toArray(ans);
+    }
+
+
+    public int[][] mergeOld(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         int n = intervals.length;
         Stack<int[]> stack = new Stack<>();
         stack.push(intervals[0]);
@@ -28,15 +46,15 @@ public class Solution_56 {
             int[] peek = stack.peek();
             if (intervals[i][0] <= peek[1]) {
                 stack.pop();
-                int end = peek[1]<intervals[i][1]?intervals[i][1]:peek[1];
+                int end = peek[1] < intervals[i][1] ? intervals[i][1] : peek[1];
                 stack.push(new int[]{peek[0], end});
-            }else{
+            } else {
                 stack.push(intervals[i]);
             }
             i++;
         }
 //        List<int[]> list = stack.reversed().reversed();
-        int[][] ans  = new int[stack.size()][];
+        int[][] ans = new int[stack.size()][];
         return stack.toArray(ans);
     }
 }
