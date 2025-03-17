241f4d24b28a07fe87f3ecf5a1666fca9270ebd4
whywhathow <whywhathow.fun@gmail.com>
2025-03-17 10:11:58 +0800

    #medium #greedy #stupid

---
+++ offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_1963.java
@@ -0,0 +1,53 @@
+package leetcode.algorithm.greedy;
+
+/**
+ * @program: LeetCodeSolution
+ * @description: #
+ * @author: WhyWhatHow
+ **/
+
+public class Solution_1963 {
+
+    public static void main(String[] args) {
+        Solution_1963 sol = new Solution_1963();
+//        System.out.println(sol.minSwaps("]]][[["));
+        System.out.println(sol.minSwaps("[[[]]]][][]][[]]][[["));
+        System.out.println("==================");
+    }
+
+    public int minSwaps(String s) {
+        char[] cs = s.toCharArray();
+        int l = 0, r = cs.length - 1;
+        int mid = cs.length / 2;
+        int ans = 0;
+        int c = 0;// '[' 数量
+        for (; l < cs.length; l++) {
+            if (cs[l] == ']') {
+                if (c > 0) {
+                    c--;
+                    continue;
+                }
+                ans++;
+                c = 1;
+
+                while (r >= mid) {
+                    if (cs[r] == '[') {
+                        swap(cs, l, r);
+                        r--;
+                        break;
+                    }
+                    r--;
+                }
+            } else c++;
+        }
+        return ans;
+    }
+
+    private void swap(char[] cs, int l, int r) {
+        char c = cs[l];
+        cs[l] = cs[r];
+        cs[r] = c;
+    }
+}
+
+
