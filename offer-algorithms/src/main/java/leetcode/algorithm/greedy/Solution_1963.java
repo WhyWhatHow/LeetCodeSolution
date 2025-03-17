1d147353da190a7dce8dffd227aa09cb82b5fccc
whywhathow <whywhathow.fun@gmail.com>
2025-03-17 10:24:40 +0800

    refactor: #optimize

--- offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_1963.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_1963.java
@@ -17,16 +17,14 @@ public class Solution_1963 {
 
     public int minSwaps(String s) {
         char[] cs = s.toCharArray();
-        int l = 0, r = cs.length - 1;
-        int mid = cs.length / 2;
         int ans = 0;
         int c = 0;// '[' 数量
-        for (; l < cs.length; l++) {
-            if (cs[l] == '[') c++;
+        for (char a : cs) {
+            if (a == '[') c++;
             else if (c > 0) c--;
             else {
-                c = 1; // 默认寻找最右侧的[, 如果不交换, 最终结果会是c=ans; 因为多了ans 个[.
                 ans++;
+                c++;
             }
         }
         return ans;
