0bc029faafc68f3e5f871e735f653c11ec2b81a8
whywhathow <whywhathow.fun@gmail.com>
2025-03-24 08:49:31 +0800

    fix : some testcase filled.

--- offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_2116.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_2116.java
@@ -21,6 +21,7 @@ public class Solution_2116 {
     /**
      * 所谓的前缀匹配, 即本例中的() , 如果满足题意要求的 ,那么任意前缀子字符串 的结果会 保证 左括号的数量 会≥ 右括号的数量.
      * 同理, 在后缀匹配时, 也已就按照本例() ,  也可以得出同样的结论 右括号的数量>=左括号的数量.
+     *
      * @param s
      * @param locked
      * @return
@@ -32,19 +33,22 @@ public class Solution_2116 {
 
         int cnt = 0;
 
-        //left -> right , check ( is fill all )
+        //left -> right , the number of ( >= the number of )
         for (int i = 0; i < cs.length; i++) {
             if (cs[i] == '(' || locked.charAt(i) == '0') cnt++;
             else cnt--;
+            if (cnt < 0) return false;
         }
 
         int cc = 0;
-        // right -> left  check ) is fill (
+        // right -> left  , the number of ) >= the number of (
         for (int i = cs.length - 1; i >= 0; i--) {
             if (cs[i] == ')' || locked.charAt(i) == '0') cc++;
             else cc--;
+            if (cc < 0) return false;
         }
-        return cc >= 0 && cnt >= 0;
+
+        return true;
 
 
     }
