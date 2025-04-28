6a7ca90ef910d0d7bd4adeb166a2f2252464f442
whywhathow <whywhathow.fun@gmail.com>
2025-04-28 22:18:51 +0800

    #dp #medium

--- offer-algorithms/src/main/java/leetcode/algorithm/dp/Solution_152.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/dp/Solution_152.java
@@ -1,4 +1,4 @@
-package leetcode.algorithm.demo;
+package leetcode.algorithm.dp;
 
 /**
  * @program: LeetCodeSolution
@@ -7,15 +7,32 @@ package leetcode.algorithm.demo;
  **/
 
 public class Solution_152 {
+
+    // f[0]
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int max = nums[0], min = nums[0];// max,min 表示 [0,i-1] cur idx=i 的最大值,以及最小值.
+        int res = max; // 表示全部的最大值,最小值.
+        for (int i = 1; i < nums.length; i++) {
+            int tmp = max;
+            max = Math.max(nums[i], Math.max(max * nums[i], min * nums[i]));
+            min = Math.min(nums[i], Math.min(tmp * nums[i], min * nums[i]));
+            res = Math.max(max, res);
+
+        }
+        return res;
+
+    }
+
     /**
      * 子数组最少可以有一个元素,也就是自身.
      * 设 dp[i] 为i位置的最大乘积
-        * dp[i] = Math.max(nums[i], dp[i - 1] * nums[i]);
+     * dp[i] = Math.max(nums[i], dp[i - 1] * nums[i]);
      *
      * @param nums
      * @return
      */
-    public int maxProduct(int[] nums) {
+    public int maxProductFail(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
