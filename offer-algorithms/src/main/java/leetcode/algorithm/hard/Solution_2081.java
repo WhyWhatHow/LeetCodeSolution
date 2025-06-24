99401c38cba9b2bbff627b4d8bd1a27d95d93152
whywhathow <whywhathow.fun@gmail.com>
2025-06-24 17:04:13 +0800

    #hard #回文 doc: 不使用打表,而是对应每一次处理函数进行处理, 算是一次测试吧.

--- offer-algorithms/src/main/java/leetcode/algorithm/hard/Solution_2081.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/hard/Solution_2081.java
@@ -1,9 +1,6 @@
 package leetcode.algorithm.hard;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 /**
  * @program: LeetCodeSolution
  * @description: #
@@ -19,68 +16,116 @@ public class Solution_2081 {
         System.out.println(Integer.toString(121, 3));
         System.out.println("==================");
     }
-
-    static ArrayList<Long>[] ans = new ArrayList[10]; // 2-9 进制 每一个合格的元素数.
-    static boolean inited = false;
-
-    static void init() {
-        if (inited) return;
-        inited = true;
-        Arrays.setAll(ans, i -> new ArrayList<>()); //
-
-
-        for (int base = 1; ; base *= 10) {
-            if (base >= 1000_000) break;
-            //  gen  奇数个的回文数 10进制中, 比如base = 10 , 那么结果就是 [101,999]
-            // odd , 121-> prefix = 12, 保留1
+    public long kMirror(int k, int n) {
+        long res = 0;
+        int cnt = 0;
+        for (int base = 1;; base *= 10) {
+            if(base >= 1000_000) break;
+            // odd gen , example: base =10 , res 101, 999
             for (int i = base; i < base * 10; i++) {
                 long x = i;
-                for (int t = i / 10; t > 0; t /= 10) {
+                for (int t = i / 10; t > 0; t /= 10) // prefix 122, -> 12221
                     x = x * 10 + t % 10;
+                if (check(x, k)) {
+                    res += x;
+                    cnt++;
                 }
-                doHandle(x);
+                if (cnt == n)
+                    break;
             }
 
-            // gen  偶数位 的回文数, 前缀是base
+            // even gen example: base=10 , res [1001, 9999]
             for (int i = base; i < base * 10; i++) {
-                long x = i; // example 12, -> 1221
-                for (int t = i; t > 0; t /= 10) {
+                long x = i;
+                for (int t = i; t > 0; t /= 10) { // example t= 12, -> 1221
                     x = x * 10 + t % 10;
                 }
-                doHandle(x);
+                if (check(x, k)) {
+                    res += x;
+                    cnt++;
+                }
+                if (cnt == n)
+                    break;
             }
         }
 
-
+        return res;
     }
 
-    // 判断是否是 x 是否是 k进制下的 回文, 如果是, 则加入
-    private static boolean doHandle(long x) {
-        for (int k = 2; k < 10; k++) {
-            // ISSUE [whywhathow]  [must] size 的大小要如何确定呢?
-            if (ans[k].size()<30 && check(x, k)) {
-                ans[k].add(x);
-            }
+    boolean check(long x, int k) {
+        var s = Long.toString(x, k);
+        char[] cs = s.toCharArray();
+        int l = 0, r = cs.length - 1;
+        while (l <= r) {
+            if (cs[l] != cs[r])
+                return false;
+            l++;
+            r--;
         }
         return true;
     }
 
-    // 判断x 在k进制下是否是回文.
-    private static boolean check(long x, int k) {
-        String s = Long.toString(x, k);
-        return s.equals(new StringBuilder(s).reverse().toString());
-    }
-
-    long res;
-
-    public long kMirror(int k, int n) {
-        res = 0;
-        init();
-        for (int i = 0; i < n; i++) {
-            res += ans[k].get(i);
-        }
-        return res;
-    }
+//    static ArrayList<Long>[] ans = new ArrayList[10]; // 2-9 进制 每一个合格的元素数.
+//    static boolean inited = false;
+//
+//    static void init() {
+//        if (inited) return;
+//        inited = true;
+//        Arrays.setAll(ans, i -> new ArrayList<>()); //
+//
+//
+//        for (int base = 1; ; base *= 10) {
+//            if (base >= 1000_000) break;
+//            //  gen  奇数个的回文数 10进制中, 比如base = 10 , 那么结果就是 [101,999]
+//            // odd , 121-> prefix = 12, 保留1
+//            for (int i = base; i < base * 10; i++) {
+//                long x = i;
+//                for (int t = i / 10; t > 0; t /= 10) {
+//                    x = x * 10 + t % 10;
+//                }
+//                doHandle(x);
+//            }
+//
+//            // gen  偶数位 的回文数, 前缀是base
+//            for (int i = base; i < base * 10; i++) {
+//                long x = i; // example 12, -> 1221
+//                for (int t = i; t > 0; t /= 10) {
+//                    x = x * 10 + t % 10;
+//                }
+//                doHandle(x);
+//            }
+//        }
+//
+//
+//    }
+//
+//    // 判断是否是 x 是否是 k进制下的 回文, 如果是, 则加入
+//    private static boolean doHandle(long x) {
+//        for (int k = 2; k < 10; k++) {
+//            // ISSUE [whywhathow]  [must] size 的大小要如何确定呢?
+//            if (ans[k].size()<30 && check(x, k)) {
+//                ans[k].add(x);
+//            }
+//        }
+//        return true;
+//    }
+//
+//    // 判断x 在k进制下是否是回文.
+//    private static boolean check(long x, int k) {
+//        String s = Long.toString(x, k);
+//        return s.equals(new StringBuilder(s).reverse().toString());
+//    }
+//
+//    long res;
+//
+//    public long kMirror(int k, int n) {
+//        res = 0;
+//        init();
+//        for (int i = 0; i < n; i++) {
+//            res += ans[k].get(i);
+//        }
+//        return res;
+//    }
 
 
 }
