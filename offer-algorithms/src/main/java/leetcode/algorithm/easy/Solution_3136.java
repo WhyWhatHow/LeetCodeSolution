a6bcb2e4dca8e130fa4febf5edfdadde42f43b84
whywhathow <whywhathow.fun@gmail.com>
2025-07-15 08:41:05 +0800

    #easy doc: 请仔细读题.

---
+++ offer-algorithms/src/main/java/leetcode/algorithm/easy/Solution_3136.java
@@ -0,0 +1,40 @@
+package leetcode.algorithm.easy;
+
+import java.util.Set;
+
+/**
+ * @program: LeetCodeSolution
+ * @description: #
+ * @author: WhyWhatHow
+ **/
+
+public class Solution_3136 {
+
+    public static void main(String[] args) {
+        Solution_3136 sol = new Solution_3136();
+
+        System.out.println("==================");
+    }
+
+    class Solution {
+        public boolean isValid(String word) {
+            char[] cs = word.toLowerCase().toCharArray();
+            boolean y =false , yy =false ;
+            int cnt = 0 ;
+            Set<Character> s = Set.of('a', 'e', 'i', 'o', 'u');
+            Set<Character> ss = Set.of('$', '@', '#');
+            for(char c: cs){
+                if(ss.contains(c)) return false ;
+                if(c>='a' && c<='z'){
+                    if(s.contains(c)) y = true;
+                    else yy =true;
+
+                }
+                cnt++ ;
+            }
+            return y && yy && cnt>=3;
+        }
+    }
+}
+
+
