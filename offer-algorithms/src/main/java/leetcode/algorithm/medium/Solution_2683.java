fb5915f690fe574e8778f49f22aeb3575e2b82d6
whywhathow <whywhathow.fun@gmail.com>
2025-07-31 15:31:01 +0800

    optimize: 根据异或运算+求和 +参考答案的点播后的优化版本,old version is so stupid.

--- offer-algorithms/src/main/java/leetcode/algorithm/medium/Solution_2683.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/medium/Solution_2683.java
@@ -14,13 +14,33 @@ public class Solution_2683 {
         System.out.println("==================");
     }
 
+    /**
+     * hint : 异或可以去括号, 不改变优先级.
+     * 设初始数组是a, 目标数组是b,根据题意可得
+     * b0 = a0 ^ a1
+     * b1 = a1 ^ a2
+     * ....
+     * bn = an ^a0
+     * b0 ^ b1 ^ b2 ^...^bn = (a0 ^ a1) ^ (a1 ^ a2) ^(a2 ^a3) ^ ...^(an ^ a0)
+     * = 0
+     * 也就是说,我们只需要判断b数组的异或和是否为0即可.
+     *
+     * @param derived
+     * @return
+     */
     public boolean doesValidArrayExist(int[] derived) {
-        int[] os = new int[derived.length];
-
-        // check os[0] = 0 , os[0] =1 ;
-        return check(os, 0, derived) || check(os, 1, derived);
+        int sum = derived[0];
+        for (int i = 1; i < derived.length; i++) {
+            sum ^= derived[i];
+        }
+        return sum == 0;
+//        int[] os = new int[derived.length];
+//
+//        // check os[0] = 0 , os[0] =1 ;
+//        return check(os, 0, derived) || check(os, 1, derived);
     }
 
+    // 还原之前的origin 数组,再判断是否可以合成derived 数组.
     private boolean check(int[] os, int val, int[] derived) {
         os[0] = val;
         int n = derived.length;
