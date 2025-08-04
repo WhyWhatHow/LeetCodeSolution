9f6be04d3688d54e71d3fa7a2fb810cc96fd716d
whywhathow <whywhathow.fun@gmail.com>
2025-08-04 08:18:23 +0800

    #hard #presum #slide_window -  using TreeMap for efficient range queries

--- offer-algorithms/src/main/java/leetcode/algorithm/hard/Solution_2106.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/hard/Solution_2106.java
@@ -1,5 +1,7 @@
 package leetcode.algorithm.hard;
 
+import java.util.TreeMap;
+
 /**
  * @program: LeetCodeSolution
  * @description: #
@@ -11,23 +13,65 @@ public class Solution_2106 {
     public static void main(String[] args) {
         Solution_2106 sol = new Solution_2106();
         System.out.println(sol.maxTotalFruits(new int[][]{
-//                        {2, 8}, {6, 3}, {8, 6}
+                        {2, 8}, {6, 3}, {8, 6}
 //                        {2000_00,1000}
-                        {0, 10000}
+//                        {0, 10000}
                 },
-//                5, 4
+                5, 4
 //                0,2000_00
-                2000_00, 2000_00
+//                2000_00, 2000_00
         ));
         System.out.println("==================");
     }
 
+    // 统计 从[startPos-k,startPos] -> [startPos, startPos+k] 之间的最大值.
+    // 使用treemap 解题 , key 表示位置, val: 表示[0,i] range内所有的水果.
+    // 对于[i,j] 区间而言,  map.floor(j) - map.floor(i-1)
+    // 注意: 由于需要 0这个位置,需要默认放置{-1,0} 作为下界. 以及 {2000_00, presum} 作为上界(貌似可以省略).
+    // map{key : pos, val means [0,i] pre_sum ;}
+    // [i,j]  map[j]- map[i-1]
 
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int sum = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>(); //  key: position , val: [0,key] val;
+        map.put(-1, 0);
+        for (int[] f : fruits) {
+            sum += f[1];
+            map.put(f[0], sum);
+        }
+        int MAX = 2000_00;
+        map.put(MAX, sum); //
+
+        int res = 0;
+        // go to left
+        for (int l = 0; l <= k; l++) {
+            int lp = startPos - l;
+            lp = Math.max(0, lp);
+            int r = k - l;
+            int rp = lp + r;
+            rp = Math.min(rp, MAX);
+            res = Math.max(map.floorEntry(rp).getValue() - map.floorEntry(lp - 1).getValue(), res);
+        }
+
+//        right
+        for (int r = 0; r <= k; r++) {
+            int rp = startPos + r;
+            rp = Math.min(rp, MAX);
+            int l = k - r;
+            int lp = rp - l;
+            lp = Math.max(0, lp);
+            res = Math.max(map.floorEntry(rp).getValue() - map.floorEntry(lp - 1).getValue(), res);
+        }
+        return res;
+    }
+
+
+    //  数组
     // 统计 从[startPos-k,startPos] -> [startPos, startPos+k] 之间的最大值.
     // 设 f[i] 表示 [0,i) 范围内的果子之和.
     // [0,i] 范围的前缀和, [i,j] f[j]-f[i-1] ,如果要表示0 , 需要整体右移一格, 不方便思考.
     // 则 区间[i,j]内的果子和则是:   f[j+1]-f[i]
-    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+    public int maxTotalFruitsByArray(int[][] fruits, int startPos, int k) {
         int n = fruits.length;
         int MAX = 2000_00;
         int[] dp = new int[2000_05];
