a31c77ec489e989300e8b38a59c44eb92dbdb53f
whywhathow <whywhathow.fun@gmail.com>
2025-08-06 08:38:49 +0800

    refactor(leetcode): optimize code structure and improve readability

--- offer-algorithms/src/main/java/leetcode/algorithm/segment/Solution_3479.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/segment/Solution_3479.java
@@ -25,28 +25,16 @@ public class Solution_3479 {
     public int numOfUnplacedFruits(int[] f, int[] b) {
         int n = f.length;
         int m = b.length;
+        boolean[] v = new boolean[m];
         // 把 b ->分成大小为t的块,每一块存其最大值, 这样就可以快速过滤.
         int t = (int) Math.sqrt(m);
-        boolean[] v = new boolean[m];
-        ArrayList<int[]> list = new ArrayList<>(); // int[] =>{ max , cnt 标记可以访问的元素数量. )
-        int cnt = 0;
-        int max = 0;
-        for (int i = 0; i < b.length; i++) {
-            max = Math.max(max, b[i]);
-            cnt++;
-            if (cnt == t) {
-                list.add(new int[]{max, cnt});
-                max = 0;
-                cnt = 0;
-            }
-        }
-        list.add(new int[]{max, cnt});
-
+        var list = buildSegmentList(t, b);
         for (int tar : f) {
             for (int i = 0; i < list.size(); i++) {
                 if (tar <= list.get(i)[0] && list.get(i)[1] > 0) {
-                    int st = i * t;
+
                     boolean yes = false;
+                    int st = i * t;
                     int end = Math.min(st + t, m);
                     int cntt = list.get(i)[1];
                     for (int k = st; k < end; k++) {
@@ -67,6 +55,24 @@ public class Solution_3479 {
 
         return n;
     }
+
+    private ArrayList<int[]> buildSegmentList(int t, int[] b) {
+        ArrayList<int[]> list = new ArrayList<>(); // int[] =>{ max , cnt 标记可以访问的元素数量. )
+        int cnt = 0;
+        int max = 0;
+        for (int i = 0; i < b.length; i++) {
+            max = Math.max(max, b[i]);
+            cnt++;
+            if (cnt == t) {
+                list.add(new int[]{max, cnt});
+                max = 0;
+                cnt = 0;
+            }
+        }
+        list.add(new int[]{max, cnt});
+        return list;
+    }
+
 
 }
 
