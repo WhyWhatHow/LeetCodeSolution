7f0968f749bcca9ef48a3b1e6306739286679651
whywhathow <whywhathow.fun@gmail.com>
2025-10-07 09:29:34 +0800

    #medium #greedy

--- offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_1488.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/greedy/Solution_1488.java
@@ -1,4 +1,4 @@
-package leetcode.algorithm.demo;
+package leetcode.algorithm.greedy;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -23,19 +23,54 @@ public class Solution_1488 {
         set.add(3);
         Integer ceiling = set.ceiling(2);
         System.out.println("==================");
-        int[] ints = sol.avoidFlood2(new int[]{
+//        int[] ints = sol.avoidFlood2(new int[]{
+        int[] ints = sol.avoidFloodIn20251007(new int[]{
 //                1, 2, 3, 4
-//                1, 2, 0, 0, 2, 1
+                1, 2, 0, 0, 2, 1
 //                1,0,1,0,2,0,2
 //                1, 0, 2, 0, 2, 1
 //                1, 2, 0, 2, 3, 0, 1
 //                1, 0, 2, 3, 0, 1, 2
 //                69, 0, 0, 0, 69
-                1, 0, 2, 0, 3, 0, 2, 0, 0, 0, 1, 2, 3
+//                1, 0, 2, 0, 3, 0, 2, 0, 0, 0, 1, 2, 3
         });
         System.out.println(ints);
 
     }
+
+    // greedy : 对于会发生flood 的lake,假设它的下雨天分别是day1,day2, 只需要在[day1+1,day2-1] 找到任意一个不下雨的天把其抽干即可,这里选择第一个>day1 且不下雨的天进行抽水.
+    public int[] avoidFloodIn20251007(int[] rains) {
+        int n = rains.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        var set = new TreeSet<Integer>(); //  days
+        HashMap<Integer, Integer> map = new HashMap<>(); // key : lake_num , val : day
+        boolean yes = true;
+        for (int i = 0; i < rains.length; i++) {
+            if (rains[i] == 0) {
+                set.add(i);
+                continue;
+            }
+            if (map.containsKey(rains[i])) { // repeat -> if we can't handle it, no answer.
+                Integer day = map.get(rains[i]);
+                Integer idx = set.ceiling(day);
+                if (idx == null) {
+                    yes = false;
+                    break;
+                }
+                set.remove(idx);
+                ans[idx] = rains[i];
+
+            }
+            map.put(rains[i], i);
+        }
+        while (!set.isEmpty()) {
+            ans[set.pollFirst()] = 1;
+        }
+
+        return yes ? ans : new int[]{};
+    }
+
 
     public int[] avoidFlood2(int[] rains) {
         int[] ans = new int[rains.length];
@@ -57,7 +92,7 @@ public class Solution_1488 {
                 }
                 ans[idx] = rains[i];
                 set.remove(idx);
-//                map.remove(rains[i]); // don't need remove, cause it will be replaced soon.
+                //                map.remove(rains[i]); // don't need remove, cause it will be replaced soon.
             }
             map.put(rains[i], i);
 
