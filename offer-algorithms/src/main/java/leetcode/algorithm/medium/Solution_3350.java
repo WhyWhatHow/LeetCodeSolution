1b8a88e7e82933677bbc6a55842ee07607f98906
whywhathow <whywhathow.fun@gmail.com>
2025-10-15 12:07:26 +0800

    #medium

---
+++ offer-algorithms/src/main/java/leetcode/algorithm/medium/Solution_3350.java
@@ -0,0 +1,62 @@
+package leetcode.algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: LeetCodeSolution
+ * @description: #
+ * @author: WhyWhatHow
+ **/
+
+public class Solution_3350 {
+
+    public static void main(String[] args) {
+        Solution_3350 sol = new Solution_3350();
+
+        System.out.println(sol.maxIncreasingSubarrays(List.of(
+//                2, 5, 7, 8, 9, 2, 3, 4, 3, 1
+                1, 2, 3, 4, 4, 4, 4, 5, 6, 7
+        )));
+        System.out.println("==================");
+    }
+
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        int[] f = new int[n]; // f[i] means[ f[i], i] range 递增.
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                f[i] = f[i - 1];
+            } else {
+                f[i] = i;
+            }
+        }
+        int maxK = 0;
+        int cnt = 0;
+//        int prev = 0;
+        var q = new ArrayList<Integer>();
+        for (int i = 0; i < f.length; i++) {
+            if (i == 0) {
+                cnt = 1;
+                continue;
+            }
+            if (f[i] == f[i - 1]) {
+                cnt++;
+//                maxK= Math.max(cnt/2,)
+            } else {
+                q.add(cnt);
+                cnt = 1;
+            }
+        }
+        q.add(cnt);
+
+        maxK = q.get(0) / 2;
+        for (int i = 1; i < q.size(); i++) {
+            maxK = Math.max(maxK, Math.min(q.get(i - 1), q.get(i)));
+        }
+        return maxK;
+    }
+
+}
+
+
