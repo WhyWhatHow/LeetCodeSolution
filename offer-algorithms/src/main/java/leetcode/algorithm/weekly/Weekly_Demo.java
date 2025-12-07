24c472ee96497cac2fb042e8116b04fa8de0a806
whywhathow <whywhathow.fun@gmail.com>
2025-12-07 18:07:33 +0800

    #demo

---
+++ offer-algorithms/src/main/java/leetcode/algorithm/weekly/Weekly_Demo.java
@@ -0,0 +1,14 @@
+package leetcode.algorithm.weekly;
+
+/**
+ * @program: LeetCodeSolution
+ * @description:
+ * @author: WhyWhatHow
+ * @create: 2025-12-07 10:29
+ **/
+public class Weekly_Demo {
+    public static void main(String[] args) {
+        Weekly_Demo sol = new Weekly_Demo();
+        System.out.println("---------------------");
+    }
+}
