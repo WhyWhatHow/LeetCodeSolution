5494d68196d85ad0c9ef21b004c01adde78a147e
whywhathow <whywhathow.fun@gmail.com>
2026-05-17 06:28:08 +0800

    #medium #bfs

--- offer-algorithms/src/main/java/leetcode/algorithm/dfs/Solution_1306.java
+++ offer-algorithms/src/main/java/leetcode/algorithm/dfs/Solution_1306.java
@@ -1,5 +1,6 @@
 package leetcode.algorithm.dfs;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -12,15 +13,49 @@ public class Solution_1306 {
 
     public static void main(String[] args) {
         Solution_1306 sol = new Solution_1306();
-        System.out.println(sol.canReach(new int[]{
-//                        4, 2, 3, 0, 3, 1, 2
-                        3, 0, 2, 1, 2
-                },
-//                5
-//                0
-                2
+        System.out.println(sol.canReach20260517(
+//                new int[]{
+////                        4, 2, 3, 0, 3, 1, 2
+//                        3, 0, 2, 1, 2
+//                },
+////                5
+////                0
+//                2
+                new int[]{4, 2, 3, 0, 3, 1, 2},
+                5
         ));
         System.out.println("==================");
+    }
+
+
+    public boolean canReach20260517(int[] arr, int start) {
+
+        int n = arr.length;
+        boolean[] v = new boolean[n];
+        var q = new ArrayList<Integer>();
+        q.add(start);
+        v[start] = true;
+        if (arr[start] == 0) return true;
+        while (!q.isEmpty()) {
+            var i = q.removeFirst();
+            // i+arr[i]
+            int r = i + arr[i];
+            if (r < n && !v[r]) {
+                if (arr[r] == 0)
+                    return true;
+                q.add(r);
+                v[r] = true;
+            }
+            // i-arr[i]
+            int l = i - arr[i];
+            if (l >= 0 && !v[l]) {
+                if (arr[l] == 0)
+                    return true;
+                q.add(l);
+                v[l] = true;
+            }
+        }
+        return false;
     }
 
     boolean res = false;
